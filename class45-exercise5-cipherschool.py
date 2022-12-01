250e3812c95d3d7317a6cc55475f0ee4ab328027
ydvdhrj <117137830+ydvdhrj@users.noreply.github.com>
2022-12-01 23:32:00 +0530

    Delete class45-exercise5-cipherschool.py

--- class45-exercise5-cipherschool.py
+++
@@ -1,9 +0,0 @@
-# Exercise, Number Guessing Game
-winning_number=7
-num=int(input("Enter any number:"))
-if num==winning_number:
-    print("YOU WIN!!!")
-elif num>winning_number:
-    print("too high")
-elif num<winning_number:
-    print("too low")
